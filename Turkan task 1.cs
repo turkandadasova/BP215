38ff32bc822633217924cffd58593c496bf19049
Your Name <you@example.com>
2024-10-09 11:28:52 +0400

    first commit

---
+++ Turkan task 1.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 8, 97, 46, 67 };
+            int a = Min(numbers);
+            Console.WriteLine(a);
+
+            static int Min(int[] numbers)
+            {
+
+                int minValue = numbers.Min();
+                return minValue;
+
+
+            }
+
+
+
+        }
+
+    }
+}
